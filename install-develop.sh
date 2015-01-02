1d1cb41714b6f1dea83d805291cd30e6750fc419
Nicolargo <nicolas@nicolargo.com>
2015-01-02 23:05:58 +0100

    Add PyStache lib

--- install-develop.sh
+++ install-develop.sh
@@ -72,7 +72,7 @@ fi
 shopt -u nocasematch
 
 # Install libs
-do_with_root pip install psutil bottle batinfo https://bitbucket.org/gleb_zhulik/py3sensors/get/tip.tar.gz zeroconf netifaces pymdstat influxdb statsd
+do_with_root pip install psutil bottle batinfo https://bitbucket.org/gleb_zhulik/py3sensors/get/tip.tar.gz zeroconf netifaces pymdstat influxdb statsd pystache
 
 # Install or ugrade Glances from the Git develop repository
 git clone -b develop https://github.com/nicolargo/glances.git
